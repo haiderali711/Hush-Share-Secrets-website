6c60466a8b776cd19eae0446fd6a770593480b58
armin2118 <armin2118@icloud.com>
2019-09-19 20:38:47 +0200

    fix breaking code

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ var history = require('connect-history-api-fallback');
 var userController = require('./routes/users');
 var diaryController = require('./routes/diaries');
 var categoryController = require('./routes/categories');
+var commentsController = require('./routes/comments');
+var storiesController = require('./routes/stories');
 
 // Variables
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/anonymous';
@@ -41,6 +43,8 @@ app.get('/api', function(req, res) {
 app.use('/api/users', userController);
 app.use('/api/diaries', diaryController);
 app.use('/api/categories', categoryController);
+app.use('/api/comments', commentsController);
+app.use('/api/stories', storiesController);
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function(req, res) {
